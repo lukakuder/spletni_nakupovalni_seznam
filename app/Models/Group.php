e7c036bbc0f0f6e24c874c534870ed6b928da5de
kuderluka <luka.kuder@aspiria.com>
2024-11-16 15:12:49 +0100

    Added groups and many to many relation implemented with pivot table

---
+++ app/Models/Group.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Notifications\Notifiable;
+
+class Group extends Model
+{
+    use HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'description'
+    ];
+
+    /**
+     * The users that belong to the group.
+     */
+    public function users(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class, 'group_user');
+    }
+
+    /**
+     * The lists that belong to the group.
+     */
+    protected function lists(): HasMany
+    {
+        return $this->hasMany(ShoppingList::class);
+    }
+}
