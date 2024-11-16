b81c4651bdb7afaf961282bcf75b198ca5b9bbb7
kuderluka <luka.kuder@aspiria.com>
2024-11-16 15:13:05 +0100

    Added shopping list model and migration

---
+++ database/migrations/2024_11_16_101111_create_shopping_lists_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('shopping_lists', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->nullable()->constrained()->onDelete('cascade');
+            $table->foreignId('group_id')->nullable()->constrained()->onDelete('cascade');
+
+            //If this is true, then the list belongs to a group, otherwise it belong solely to a user
+            $table->boolean('belongs_to_a_group');
+
+            $table->string('name');
+            $table->text('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('shopping_lists');
+    }
+};
