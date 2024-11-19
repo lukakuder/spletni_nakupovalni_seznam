fecfbea2558d0fc9d8a6ee0df7f8850a454643b1
enejkr <enejkr@gmail.com>
2024-11-19 19:38:17 +0100

    Update 0001_01_01_000000_create_users_table.php

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->string('profile_picture')->nullable();
+            $table->string('profile_picture')->default("profile_pictures/default-avatar.png");
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
