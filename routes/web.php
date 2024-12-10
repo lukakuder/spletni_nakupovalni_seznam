31cd6177255f12e15aa72708fbe51c43f20329f1
lovroobreza <141015983+obrezalovro@users.noreply.github.com>
2024-12-10 17:31:07 +0100

    fix paths

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,9 @@ Route::middleware('auth')->group(function () {
 Route::middleware('auth')->group(function () {
     Route::get('lists', [ProfileController::class, 'myLists'])->name('user.lists');
 
+    Route::get('lists/create', [ListController::class, 'create'])->name('lists.create');
+    Route::post('lists/store', [ListController::class, 'store'])->name('lists.store');
+
     Route::get('lists/{id}', [ListController::class, 'show'])->name('lists.show');
     Route::post('lists/{id}/items', [ListController::class, 'storeItem'])->name('lists.items.store');
 
@@ -28,8 +31,6 @@ Route::middleware('auth')->group(function () {
     Route::get('/lists/{id}/export', [ListController::class, 'export'])->name('lists.export');
     Route::patch('/lists/{id}/reminder', [ListController::class, 'updateReminder'])->name('lists.updateReminder');
 
-    Route::get('lists/create', [ListController::class, 'create'])->name('lists.create');
-    Route::post('lists/store', [ListController::class, 'store'])->name('lists.store');
 });
 Route::middleware('auth')->group(function () {
     Route::get('groups', [ProfileController::class, 'myGroups'])->name('user.groups');
