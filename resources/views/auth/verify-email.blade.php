0a94f6d6b9453ce904a58d74d19e82462b855483
enejkr <enejkr@gmail.com>
2025-01-11 17:09:56 +0100

    prevod v slo
    w

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -1,13 +1,14 @@
 <x-guest-layout>
     <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
-        {{ __('Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another.') }}
+        {{ __('Hvala za prijavo! Preden začnete, prosimo, da potrdite svoj e-poštni naslov s klikom na povezavo, ki smo vam jo pravkar poslali. Če e-pošte niste prejeli, vam bomo z veseljem poslali novo.') }}
     </div>
 
     @if (session('status') == 'verification-link-sent')
         <div class="mb-4 font-medium text-sm text-green-600 dark:text-green-400">
-            {{ __('A new verification link has been sent to the email address you provided during registration.') }}
+            {{ __('Nova povezava za potrditev je bila poslana na e-poštni naslov, ki ste ga navedli ob registraciji.') }}
         </div>
     @endif
+
 
     <div class="mt-4 flex items-center justify-between">
         <form method="POST" action="{{ route('verification.send') }}">
@@ -15,7 +16,7 @@
 
             <div>
                 <x-primary-button>
-                    {{ __('Resend Verification Email') }}
+                    {{ __('Ponovno pošlji verifikacisko E-poštp') }}
                 </x-primary-button>
             </div>
         </form>
