1e607c4df99767bee01c22136a8c8b43d6bed5e4
kuderluka <luka.kuder@aspiria.com>
2025-01-16 12:32:21 +0100

    Routes reworked

--- routes/web.php
+++ routes/web.php
@@ -8,53 +8,67 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\NotificationController;
 use App\Http\Controllers\HomeController;
 
-Route::get('/groups', [GroupController::class, 'index'])->name('groups.index');
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [UserController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [UserController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [UserController::class, 'destroy'])->name('profile.destroy');
-});
+    Route::get('/', [HomeController::class, 'index'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/user/lists', [ListController::class, 'getUsersLists'])->name('user.lists');
-    Route::get('/user/groups', [GroupController::class, 'index'])->name('user.groups');
+    // User
+    Route::prefix('user')->group(function () {
+        Route::get('/lists', [ListController::class, 'getUsersLists'])->name('user.lists');
+        Route::get('/groups', [GroupController::class, 'index'])->name('user.groups');
+    });
+
+    // Profile management
+    Route::prefix('profile')->group(function () {
+        Route::get('/', [UserController::class, 'edit'])->name('profile.edit');
+        Route::patch('/', [UserController::class, 'update'])->name('profile.update');
+        Route::delete('/', [UserController::class, 'destroy'])->name('profile.destroy');
+    });
+
+    // Lists
+    Route::prefix('lists')->group(function () {
+        Route::get('/', [ListController::class, 'getLists'])->name('lists');
+        Route::get('/create', [ListController::class, 'create'])->name('lists.create');
+        Route::post('/store', [ListController::class, 'store'])->name('lists.store');
+        Route::get('/{id}', [ListController::class, 'show'])->name('lists.show');
+        Route::post('/{id}/items', [ListController::class, 'storeItem'])->name('lists.items.store');
+        Route::post('/{id}/import', [ListController::class, 'import'])->name('lists.import');
+        Route::get('/{id}/export', [ListController::class, 'export'])->name('lists.export');
+        Route::get('/{id}/export-report', [ListController::class, 'export_report'])->name('lists.exportReport');
+        Route::get('/{id}/divide', [ListController::class, 'divide'])->name('lists.divide');
+        Route::post('/{id}/duplicate', [ListController::class, 'duplicate'])->name('lists.duplicate');
+        Route::patch('/{id}/reminder', [ListController::class, 'updateReminder'])->name('lists.updateReminder');
+        Route::post('/{list}/receipts', [ListController::class, 'uploadReceipt'])->name('lists.uploadReceipt');
+    });
+
+    Route::prefix('groups')->group(function () {
+        Route::get('/', [GroupController::class, 'index'])->name('groups.index');
+        Route::get('/create', [GroupController::class, 'create'])->name('groups.create');
+        Route::post('/store', [GroupController::class, 'store'])->name('groups.store');
+        Route::get('/{id}', [GroupController::class, 'show'])->name('groups.show');
+        Route::get('/{id}/lists', [GroupController::class, 'getGroupShoppingLists']);
+        Route::get('/{group}/add-members', [GroupController::class, 'addMembersForm'])->name('groups.addMembersForm');
+        Route::post('/{group}/add-members', [GroupController::class, 'addMembers'])->name('groups.addMembers');
+        Route::post('/{group}/leave', [GroupController::class, 'leave'])->name('groups.leave');
+        Route::get('/{group}/detailed-show', [GroupController::class, 'detailedShow'])->name('groups.detailedShow');
+        Route::post('/{groupId}/invite', [NotificationController::class, 'posljiPovabilo']);
+    });
+
+    Route::prefix('opozorila')->group(function () {
+        Route::get('/', [NotificationController::class, 'prikaziOpozorila'])->name('opozorila.index');
+        Route::post('/oznaci-prebrano', [NotificationController::class, 'oznaciPrebrano'])->name('opozorila.oznaciPrebrano');
+        Route::get('/stevilo-neprebranih', [NotificationController::class, 'steviloNeprebranih'])->name('opozorila.steviloNeprebranih');
+        Route::post('/sprejmi/{id}', [NotificationController::class, 'sprejmiPovabilo'])->name('opozorila.sprejmiPovabilo');
+    });
 
     Route::get('/tag/{tag}', [TagController::class, 'index'])->name('tag.show');
 
-    Route::get('/lists', [ListController::class, 'getLists'])->name('lists');
-    Route::get('/lists/create', [ListController::class, 'create'])->name('lists.create');
-    Route::post('/lists/store', [ListController::class, 'store'])->name('lists.store');
-    Route::post('/lists/{list}/receipts', [ListController::class, 'uploadReceipt'])->name('lists.uploadReceipt');
-    Route::post('/lists/{list}/receipts', [ListController::class, 'storeReceipt'])->name('lists.storeReceipt');
-
-    Route::get('/lists/{id}', [ListController::class, 'show'])->name('lists.show');
-    Route::post('/lists/{id}/items', [ListController::class, 'storeItem'])->name('lists.items.store');
-
-    Route::post('/lists/{id}/import', [ListController::class, 'import'])->name('lists.import');
-    Route::get('/lists/{id}/export', [ListController::class, 'export'])->name('lists.export');
-    Route::get('/lists/{id}/export-report', [ListController::class, 'export_report'])->name('lists.exportReport');
-    Route::get('/lists/{id}/divide', [ListController::class, 'divide'])->name('lists.divide');
-    Route::post('/lists/{id}/duplicate', [ListController::class, 'duplicate'])->middleware('auth')->name('lists.duplicate');
-
-    Route::patch('/lists/{id}/reminder', [ListController::class, 'updateReminder'])->name('lists.updateReminder');
     Route::patch('/items/{id}/mark-purchased', [ListController::class, 'markAsPurchased'])->name('items.markPurchased');
 
-    Route::get('/groups', [UserController::class, 'myGroups'])->name('user.groups');
-    Route::get('/groups/create', [GroupController::class, 'create'])->name('groups.create');
-    Route::post('/groups/store', [GroupController::class, 'store'])->name('groups.store');
-    Route::get('/groups/{id}', [GroupController::class, 'show'])->name('groups.show');
-    Route::get('/groups/{id}/lists', [GroupController::class, 'getGroupShoppingLists']);
-
-    // TODO: Kramar tole ne more met kr / route ker pokvari nas homepage
-    //Route::get('/', [HomeController::class, 'index'])->name('home');
+    // Tole je black box za mene lol kramar fix pls
     Route::middleware('auth')->get('/opozorila', [NotificationController::class, 'index'])->name('opozorila.index');
     Route::get('/opozorila', [NotificationController::class, 'prikaziOpozorila'])->name('opozorila.index');
 
@@ -66,16 +80,6 @@ Route::middleware('auth')->group(function () {
     Route::middleware('auth')->get('/opozorila/stevilo-neprebranih', [NotificationController::class, 'steviloNeprebranih'])->name('opozorila.steviloNeprebranih');
     Route::get('/opozorila/stevilo-neprebranih', [NotificationController::class, 'steviloNeprebranih'])->name('opozorila.steviloNeprebranih');
 
-    Route::get('/dashboard', [HomeController::class, 'index'])->name('dashboard');
-
-    // Prikaz obrazca za dodajanje članov
-    Route::get('/groups/{group}/add-members', [GroupController::class, 'addMembersForm'])->name('groups.addMembersForm');
-    // Shranjevanje izbranih članov v skupino
-    Route::post('/groups/{group}/add-members', [GroupController::class, 'addMembers'])->name('groups.addMembers');
-    Route::post('/groups/{group}/leave', [GroupController::class, 'leave'])->name('groups.leave');
-    Route::get('/groups', [GroupController::class, 'index'])->name('groups.index');
-    Route::get('/groups/{group}/detailed-show', [GroupController::class, 'detailedShow'])->name('groups.detailedShow');
-    Route::post('/group/{groupId}/invite', [NotificationController::class, 'posljiPovabilo'])->middleware('auth');
     Route::post('/notification/{notificationId}/accept', [NotificationController::class, 'sprejmiPovabilo'])->middleware('auth');
     Route::post('/opozorila/sprejmi/{id}', [NotificationController::class, 'sprejmiPovabilo'])->name('opozorila.sprejmiPovabilo');
 
