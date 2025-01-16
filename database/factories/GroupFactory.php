9e4f508c7c8294ac0ad1a57247b4d17b655f6cc5
David Guček <davko.gucek@gmail.com>
2025-01-16 15:47:09 +0100

    unique

--- database/factories/GroupFactory.php
+++ database/factories/GroupFactory.php
@@ -18,7 +18,7 @@ class GroupFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => $this->faker->word,
+            'name' => $this->faker->unique()->word,
             'description' => $this->faker->sentence,
         ];
     }
